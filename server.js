426b9b2e3f3d20c9e3014aebd4f89f53632a2147
Edward Howard <edward.x.howard@veeam.com>
2022-11-25 17:15:56 +0000

    Added CORS
    Added CORS support for local testing. Also updated the catalogue to an array. 
    
    Also added the ability to add an item.

--- server.js
+++ server.js
@@ -1,65 +1,61 @@
-require('dotenv').config()
+require("dotenv").config();
 
+const fs = require("fs");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
+var port = process.env.SERVER_PORT || 3001;
 
-const fs = require('fs');
-const express = require('express');
-const jwt = require('jsonwebtoken');
+// removed for local testing, uncomment before building Docker image.
+// var publicKey = fs.readFileSync(process.env.PUBLIC_KEY, "utf8");
+var routePrefix = process.env.ROUTE_PREFIX || "/catalog";
 
+const catalog = [
+  {
+    id: 1,
+    name: "sticker",
+    description: "it's a Kasten sticker",
+    price: 3,
+    imgurl: "https://www.kasten.io/hubfs/Kasten%20logos/logo-kasten.io.svg",
+    stock: 100,
+  },
+];
 
-var port = process.env.SERVER_PORT || 80;
-
-
-
-var publicKey = fs.readFileSync(process.env.PUBLIC_KEY,"utf8")
-var routePrefix = process.env.ROUTE_PREFIX || '/catalog' 
-
-
+let index = 1;
 
 function startServer() {
-    var app = express();
+  var app = express();
 
-    app.use(express.json());
+  app.use(express.json());
+  app.use(cors());
 
-    app.get("/", async (req, res) => {
-        res.status(404).send("unrecognized route")
+  app.get("/", async (req, res) => {
+    res.status(404).send("unrecognized route");
+  });
+  app.get(routePrefix + "/list", async (req, res) => {
+    res.send(catalog);
+  });
+  app.get(routePrefix + "/list/:catalogId([0-9]+)", async (req, res) => {
+    const itemSelect = req.params.catalogId;
+
+    res.send(catalog[itemSelect]);
+  });
+
+  app.post(routePrefix + "/update", async (req, res) => {
+    index += 1;
+    req.body.id = index;
+    catalog.push(req.body);
+    res.send({
+      status: "success",
     });
-    app.get(routePrefix+'/list',  async (req, res) => {
-        res.send({
-            "id":1,
-            "name":"sticker",
-            "description":"it's a Kasten sticker",
-            "price":3,
-            "imgurl":"https://www.kasten.io/hubfs/Kasten%20logos/logo-kasten.io.svg",
-            "stock":100
-        })
-    })
-    app.get(routePrefix+'/list/:catalogId([0-9]+)',  async (req, res) => {
-        res.send({
-            "id":req.params.catalogId,
-            "name":"sticker",
-            "description":"it's a Kasten sticker",
-            "price":3,
-            "imgurl":"https://www.kasten.io/hubfs/Kasten%20logos/logo-kasten.io.svg",
-            "stock":100
-        })
-    })
+  });
 
-    app.post(routePrefix+"/update", async (req, res) => {
-        res.send({
-            "status":"success"
-        })
-    })
-    
-    app.listen(port, () => {
-     console.log("Server running on port "+port);
-    });   
-    
+  app.listen(port, () => {
+    console.log("Server running on port " + port);
+  });
 }
 
+console.log("Starting fake catalog svc");
 
-console.log("Starting fake catalog svc")
-
-startServer()
-
-
+startServer();
