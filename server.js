b367c1afc6addd435b6fffd97659a11a9f50ad71
tdewin <tdewin@users.noreply.github.com>
2022-11-28 12:03:38 +0100

    init mongodb
    just implementing list

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const { MongoClient } = require('mongodb');
 
 var port = process.env.SERVER_PORT || 3001;
 
@@ -24,6 +25,25 @@ const catalog = [
 
 let index = 1;
 
+
+const clientData = {
+  "host": process.env.MONGODB_HOST || 'localhost' ,
+  "port": process.env.MONGODB_PORT || 27017,
+  "user": process.env.MONGODB_USER || 'mongoadmin',
+  "password": process.env.MONGODB_PASSWORD || 'secret',
+  "database": process.env.MONGODB_DATABASE|| 'catalog',
+};
+//"/"+clientData.database
+const connectionString = "mongodb://"+clientData.user+":"+clientData.password+"@"+clientData.host+":"+clientData.port+"?maxPoolSize=20&retryWrites=true&w=majority"
+
+const mongoClient = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let db;
+
+
 function startServer() {
   var app = express();
 
@@ -34,7 +54,10 @@ function startServer() {
     res.status(404).send("unrecognized route");
   });
   app.get(routePrefix + "/list", async (req, res) => {
-    res.send(catalog);
+    db.collection("catalog").find().toArray().then(
+      (queryResult) => { res.send(queryResult) },
+      (err) => { res.statusCode(404).send("")}
+    )
   });
   app.get(routePrefix + "/list/:catalogId([0-9]+)", async (req, res) => {
     const itemSelect = req.params.catalogId;
@@ -58,4 +81,40 @@ function startServer() {
 
 console.log("Starting fake catalog svc");
 
-startServer();
+async function run() {
+  try {
+    await mongoClient.connect();
+  
+    db = mongoClient.db(clientData.database);
+    console.log("Connected successfully to server");
+
+   
+    //some init code for if there is nothing
+    //today inserts catalog defined at beginning of file but might be swtich to configmap
+    db.collection("catalog").countDocuments().then(
+      (res) => { 
+          if (res == 0) {
+              console.log("Database is empty, init")
+              db.collection("catalog").insertMany(catalog).then(
+                  (res) => {
+                    console.log("ASYNC DB init successful")
+                  },
+                  (err) => {
+                    console.log("ASYNC Some err, initing, ignoring async call")
+                  }
+              )
+          } else {
+              console.log("ASYNC DB not empty, already inited, have to do nothing")
+          }
+          startServer()
+       },
+      (err) => { console.log("Some error in db query, not starting server",err)}
+    )
+
+    
+
+  } catch(err) {
+      console.log(err)
+  } 
+}
+run().catch(console.dir);
